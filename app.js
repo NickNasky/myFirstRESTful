e9b34e882990140acfecd027bbd736a6044c99b5
Nick Nasky <nick.nasky@gmail.com>
2017-07-26 10:50:46 -0600

    🕶 Added single ID requests

--- app.js
+++ app.js
@@ -7,12 +7,20 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(cors());
 
-app.get('/', (req,res, next) => {
+app.get('/', (req, res, next) => {
   getInfo()
   .then((info) => {
     res.json(info);
   })
 });
+
+app.get('/:id', (req, res) => {
+  let id = req.params.id;
+  getInfo().where('id', id).first()
+  .then((info) => {
+    res.json(info);
+  })
+})
 
 function getInfo() {
   return knex('*').from('homework');
